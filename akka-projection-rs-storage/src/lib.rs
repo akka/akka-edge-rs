3e7f23da003ed8f8aa853394b3d4cbcfaedb5653
huntc <huntchr@gmail.com>
2023-08-24 20:21:43 +1000

    Test the projection runner
    Found a bug along the way - we now cope with restarting the source if it disappears.

--- akka-projection-rs-storage/src/lib.rs
+++ akka-projection-rs-storage/src/lib.rs
@@ -42,65 +42,265 @@ pub async fn run<E, H, SP>(
     H: Handler<Envelope = E>,
     SP: SourceProvider<Envelope = E>,
 {
-    let mut source = source_provider
-        .source(|| async {
-            streambed_storage::load_struct(state_storage_path, secret_store, secret_path, |bytes| {
-                ciborium::de::from_reader::<StorableState, _>(bytes)
+    'outer: loop {
+        let mut source = source_provider
+            .source(|| async {
+                streambed_storage::load_struct(
+                    state_storage_path,
+                    secret_store,
+                    secret_path,
+                    |bytes| ciborium::de::from_reader::<StorableState, _>(bytes),
+                )
+                .await
+                .ok()
+                .and_then(|s| s.last_offset.map(Offset::Sequence))
             })
-            .await
-            .ok()
-            .and_then(|s| s.last_offset.map(Offset::Sequence))
-        })
-        .await;
+            .await;
 
-    let serializer = |state: &StorableState| {
-        let mut buf = Vec::new();
-        ciborium::ser::into_writer(state, &mut buf).map(|_| buf)
-    };
+        let serializer = |state: &StorableState| {
+            let mut buf = Vec::new();
+            ciborium::ser::into_writer(state, &mut buf).map(|_| buf)
+        };
 
-    let mut next_save_offset_interval = Duration::MAX;
-    let mut last_offset = None;
+        let mut next_save_offset_interval = Duration::MAX;
+        let mut last_offset = None;
 
-    loop {
-        tokio::select! {
-            envelope = source.next() => {
-                if let Some(envelope) = envelope {
-                    let Offset::Sequence(offset) = envelope.offset();
-                    if  handler.process(envelope).await.is_ok() {
-                        next_save_offset_interval = min_save_offset_interval;
-                        last_offset = Some(offset);
+        loop {
+            tokio::select! {
+                envelope = source.next() => {
+                    if let Some(envelope) = envelope {
+                        let Offset::Sequence(offset) = envelope.offset();
+                        if  handler.process(envelope).await.is_ok() {
+                            next_save_offset_interval = min_save_offset_interval;
+                            last_offset = Some(offset);
+                        }
+                    } else {
+                        break;
                     }
                 }
-            }
 
-            _ = time::sleep(next_save_offset_interval) => {
-                if last_offset.is_some() {
-                    let storable_state = StorableState {
-                        last_offset
-                    };
-                    if streambed_storage::save_struct(
-                        state_storage_path,
-                        secret_store,
-                        secret_path,
-                        serializer,
-                        rand::thread_rng,
-                        &storable_state
-                    ).await.is_err() {
-                        error!("Cannot persist offsets");
-                    }
-
+                _ = time::sleep(next_save_offset_interval) => {
                     next_save_offset_interval = Duration::MAX;
-                    last_offset = None;
+                    if last_offset.is_some() {
+                        let storable_state = StorableState {
+                            last_offset
+                        };
+                        if streambed_storage::save_struct(
+                            state_storage_path,
+                            secret_store,
+                            secret_path,
+                            serializer,
+                            rand::thread_rng,
+                            &storable_state
+                        ).await.is_err() {
+                            error!("Cannot persist offsets");
+                        }
+                        last_offset = None;
+                    }
+            }
+
+                _ = receiver.recv() => {
+                    break 'outer;
                 }
-            }
 
-            _ = receiver.recv() => {
-                break;
-            }
-
-            else => {
-                break;
+                else => {
+                    break 'outer;
+                }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashMap, env, fs, future::Future, pin::Pin};
+
+    use super::*;
+    use akka_persistence_rs::EntityId;
+    use akka_persistence_rs_commitlog::EventEnvelope;
+    use akka_projection_rs::HandlerError;
+    use async_stream::stream;
+    use async_trait::async_trait;
+    use serde::Deserialize;
+    use streambed::secret_store::{
+        AppRoleAuthReply, Error, GetSecretReply, SecretData, SecretStore, UserPassAuthReply,
+    };
+    use test_log::test;
+    use tokio::sync::mpsc;
+    use tokio_stream::Stream;
+
+    // Scaffolding
+
+    #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+    struct MyEvent {
+        value: String,
+    }
+
+    // Developers are expected to provide a marshaler of events.
+    // The marshaler is responsible for more than just the serialization
+    // of an envelope. Extracting/saving an entity id and determining other
+    // metadata is also important. We would also expect to see any encryption
+    // and decyption being performed by the marshaler.
+    // The example here overrides the default methods of the marshaler and
+    // effectively ignores the use of a secret key; just to prove that you really
+    // can lay out an envelope any way that you would like to. Note that secret keys
+    // are important though.
+
+    #[derive(Clone)]
+    struct NoopSecretStore;
+
+    #[async_trait]
+    impl SecretStore for NoopSecretStore {
+        async fn approle_auth(
+            &self,
+            _role_id: &str,
+            _secret_id: &str,
+        ) -> Result<AppRoleAuthReply, Error> {
+            panic!("should not be called")
+        }
+
+        async fn create_secret(
+            &self,
+            _secret_path: &str,
+            _secret_data: SecretData,
+        ) -> Result<(), Error> {
+            panic!("should not be called")
+        }
+
+        async fn get_secret(&self, _secret_path: &str) -> Result<Option<GetSecretReply>, Error> {
+            let mut data = HashMap::new();
+            data.insert(
+                "value".to_string(),
+                "ed31e94c161aea6ff2300c72b17741f71b616463f294dac0542324bbdbf8a2de".to_string(),
+            );
+
+            Ok(Some(GetSecretReply {
+                lease_duration: 10,
+                data: SecretData { data },
+            }))
+        }
+
+        async fn token_auth(&self, _token: &str) -> Result<(), Error> {
+            panic!("should not be called")
+        }
+
+        async fn userpass_auth(
+            &self,
+            _username: &str,
+            _password: &str,
+        ) -> Result<UserPassAuthReply, Error> {
+            panic!("should not be called")
+        }
+
+        async fn userpass_create_update_user(
+            &self,
+            _current_username: &str,
+            _username: &str,
+            _password: &str,
+        ) -> Result<(), Error> {
+            panic!("should not be called")
+        }
+    }
+
+    const MIN_SAVE_OFFSET_INTERVAL: Duration = Duration::from_millis(100);
+
+    struct MySourceProvider {
+        entity_id: EntityId,
+        event_value: String,
+    }
+
+    #[async_trait]
+    impl SourceProvider for MySourceProvider {
+        type Envelope = EventEnvelope<MyEvent>;
+
+        async fn source<F, FR>(
+            &self,
+            offset: F,
+        ) -> Pin<Box<dyn Stream<Item = Self::Envelope> + Send + 'async_trait>>
+        where
+            F: Fn() -> FR + Send + Sync,
+            FR: Future<Output = Option<Offset>> + Send,
+        {
+            Box::pin(stream! {
+                if offset().await.is_none() {
+                    yield EventEnvelope::new(self.entity_id.clone(), MyEvent { value:self.event_value.clone() }, 0);
+                    time::sleep(MIN_SAVE_OFFSET_INTERVAL * 2).await;
+                }
+            })
+        }
+    }
+
+    struct MyHandler {
+        entity_id: EntityId,
+        event_value: String,
+    }
+
+    #[async_trait]
+    impl Handler for MyHandler {
+        type Envelope = EventEnvelope<MyEvent>;
+
+        /// Process an envelope.
+        async fn process(&self, envelope: Self::Envelope) -> Result<(), HandlerError> {
+            assert_eq!(
+                envelope,
+                EventEnvelope {
+                    entity_id: self.entity_id.clone(),
+                    event: MyEvent {
+                        value: self.event_value.clone()
+                    },
+                    offset: 0
+                }
+            );
+            Ok(())
+        }
+    }
+
+    #[test(tokio::test)]
+    async fn can_run() {
+        let storage_path = env::temp_dir().join("can_run");
+        let _ = fs::remove_dir_all(&storage_path);
+        let _ = fs::create_dir_all(&storage_path);
+        println!("Writing to {}", storage_path.to_string_lossy());
+
+        // Scaffolding
+
+        let entity_id = EntityId::from("some-entity");
+        let event_value = "some value".to_string();
+
+        // Process an event.
+        let (_registration_projection_command, registration_projection_command_receiver) =
+            mpsc::channel(1);
+
+        let task_storage_path = storage_path.clone();
+        tokio::spawn(async move {
+            run(
+                &NoopSecretStore,
+                "some-secret-path",
+                &task_storage_path,
+                registration_projection_command_receiver,
+                MySourceProvider {
+                    entity_id: entity_id.clone(),
+                    event_value: event_value.clone(),
+                },
+                MyHandler {
+                    entity_id: entity_id.clone(),
+                    event_value: event_value.clone(),
+                },
+                MIN_SAVE_OFFSET_INTERVAL,
+            )
+            .await
+        });
+
+        // Wait until our storage file becomes available, which means
+        // that an event will have to have been successfully processed.
+        let mut file_found = false;
+        for _ in 0..10 {
+            if fs::metadata(&storage_path).is_ok() {
+                file_found = true;
+                break;
+            }
+            time::sleep(Duration::from_millis(100)).await;
+        }
+        assert!(file_found);
     }
 }
