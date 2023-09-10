25ddda90d7693f72dac40d2d1413f08149bbb5f9
huntc <huntchr@gmail.com>
2023-09-10 17:54:07 -0500

    Declared the Any type as per Akka's expectations

--- examples/iot-service/src/proto.rs
+++ examples/iot-service/src/proto.rs
@@ -7,6 +7,10 @@ impl Name for Registered {
     const NAME: &'static str = "Registered";
     const PACKAGE: &'static str = "iot.registration";
 
+    fn full_name() -> String {
+        format!("{}.{}", Self::PACKAGE, Self::NAME)
+    }
+
     fn type_url() -> String {
         format!("type.googleapis.com/{}", Self::full_name())
     }
@@ -18,6 +22,10 @@ include!(concat!(env!("OUT_DIR"), "/iot.temperature.rs"));
 impl Name for TemperatureRead {
     const NAME: &'static str = "TemperatureRead";
     const PACKAGE: &'static str = "iot.temperature";
+
+    fn full_name() -> String {
+        format!("{}.{}", Self::PACKAGE, Self::NAME)
+    }
 
     fn type_url() -> String {
         format!("type.googleapis.com/{}", Self::full_name())
